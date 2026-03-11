628a5c204c4b3f6dc03a650fb064db9d8554bb3c
OP-Studios14 <2025163453@ub.edu.bz>
2026-03-11 10:21:23 -0600

    Update Creates & Inserts.sql

--- Creates & Inserts.sql
+++ Creates & Inserts.sql
@@ -1,39 +1,70 @@
---Average Ticket Price by District
+CREATE TABLE VENUE (
+    VenueID SERIAL PRIMARY KEY,
+    VenueName VARCHAR(100) NOT NULL,
+    Address VARCHAR(150),
+    Phone VARCHAR(20),
+    District VARCHAR(50) NOT NULL,
+    VenueType VARCHAR(50)
+);
 
-SELECT V.district, AVG(S.price) AS AvgTicketPrice FROM SHOW_ S
-JOIN VENUE V ON S.venueID = V.venueID
-GROUP BY V.district;
+CREATE TABLE ARTIST (
+    ArtistID SERIAL PRIMARY KEY,
+    ArtistName VARCHAR(100) NOT NULL,
+    Genre VARCHAR(50),
+    District VARCHAR(50)
+);
 
---District with the Most Shows
+CREATE TABLE SHOW_ (
+    ShowID SERIAL PRIMARY KEY,
+    ShowName VARCHAR(100) NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE,
+    Price DECIMAL(8,2),
+    Description TEXT,
+    ExpectedAudience INT,
+    VenueID INT NOT NULL,
+    FOREIGN KEY (VenueID) REFERENCES VENUE(VenueID)
+);
 
-SELECT V.district, COUNT(S.ShowID) AS TotalShows FROM SHOW_ S
-JOIN VENUE V ON S.venueID = V.venueID
-GROUP BY V.district
-ORDER BY TotalShows DESC
-LIMIT 1;
+CREATE TABLE SHOW_ARTIST (
+    ShowID INT NOT NULL,
+    ArtistID INT NOT NULL,
+    PRIMARY KEY (ShowID, ArtistID),
+    FOREIGN KEY (ShowID) REFERENCES SHOW_(ShowID),
+    FOREIGN KEY (ArtistID) REFERENCES ARTIST(ArtistID)
+);
 
---Most Common Genre by District
 
-SELECT DISTINCT ON (V.District)
-V.District, A.Genre FROM SHOW_ S
-JOIN VENUE V ON S.VenueID = V.VenueID
-JOIN SHOW_ARTIST SA ON S.ShowID = SA.ShowID
-JOIN ARTIST A ON SA.ArtistID = A.ArtistID
-GROUP BY V.District, A.Genre
-ORDER BY V.District, COUNT(*) DESC;
+INSERT INTO VENUE(VenueName, Address, Phone, District, VenueType)
+VALUES
+('Santa Elena Complex', 'Santa Elena', '501-2000001', 'Cayo', 'Outdoor'),
+('Belize Civic Center', 'Belize City', '501-2000002', 'Belize', 'Arena'),
+('Dangriga Cultural Center', 'Dangriga', '501-2000003', 'Stann Creek', 'Cultural Hall'),
+('Saca Chispas Field', 'San Pedro','501-2000004', 'Belize', 'Outdoor');
 
---Venue with the Highest Average Ticket Price
+INSERT INTO ARTIST(ArtistName, Genre, District)
+VALUES
+('The Garifuna Collective', 'Paranda', 'Stann Creek'),
+('Gilharry 7', 'Belizean Brukdown', 'Orange Walk'),
+('Stig Da Artist', 'Hip Hop', 'Belmopan'),
+('Andy Palacio', 'Punta Rock', 'Toledo');
 
-SELECT V.venueName, AVG(S.price) AS AvgPrice FROM SHOW_ S
-JOIN VENUE V ON S.venueid = V.venueid
-GROUP BY V.venueName
-ORDER BY AvgPrice DESC
-LIMIT 1;
+INSERT INTO SHOW_(ShowName, StartDate, EndDate, Price, Description, ExpectedAudience, VenueID)
+VALUES
+('Cayo Summer Jam', '2026-06-20', '2026-06-20', 59.99, 'Outdoor Summer Music Festival in Cayo', 650, 1),
+('Belize City Music Fest', '2026-08-17', '2026-08-17', 79.99, 'Arena Concert feuturing multiple Belizean Artist', 4000, 2),
+('Garifuna Cultural Night','2026-11-19', '2026-11-19', 49.99, 'Celebration of Garifuna music and culture', 1000, 3),
+('Island Vibes Concert', '2026-06-27', '2026-06-27', 99.00, 'Island Foam Bash', 1200, 4);
 
---Month with the Most Shows
-
-SELECT TO_CHAR(StartDate,'Month') AS month, COUNT(*) AS show_count FROM SHOW_
-GROUP BY month
-ORDER BY show_count DESC
-LIMIT 1;
-
+INSERT INTO SHOW_ARTIST(ShowID, ArtistID)
+VALUES
+(1,1),
+(1,2),
+(2,1),
+(2,2),
+(2,3),
+(2,4),
+(3,1),
+(3,4),
+(4,1),
+(4,2);
